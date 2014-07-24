dd9102a0303dccfdea2a750f5dd15544988266fc
ilep <ivanlptr@gmail.com>
2014-07-24 13:41:15 +0200

    intégration du travail de selma

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -3,6 +3,8 @@
 Created on Mon Jul 07 19:46:37 2014
 
 @author: ivan lepoutre
+         selma hadzic
+         
 """
 
 import pandas as pd
@@ -44,21 +46,36 @@ from pylab import *
 
 # I/1 covariance between features
 
-# I/2 representtaion of the importance of features
-svc = SVC(kernel="linear", C=1) 
-rfe = RFE(estimator=svc, n_features_to_select=350, step=20) # recursive feature elimination en utilisant SVC
+# I/2 dimension reduction
+# I/2/a Recursive Feature Elimination (RFE)
+
+#SVC has difficulty to scale to dataset with more than a couple of 10000 samples
+# RFE est fitte sur 1000 digits pour etre plus rapide ds un premier temps
 Xbis = X.iloc[0:1000,:]
 ybis = y.iloc[0:1000]
-rfe.fit(Xbis,ybis) # RFE est fitte sur 1000 digits pour etre plus rapide ds un premier temps
+
+svc = SVC(kernel="linear", C=1) 
+# GridSearchCV to determine the best number of features to select
+#parameters = {'n_features_to_select':range(20,401)}
+#rfe = RFE(svc, n_features_to_select = parameters, step = 20)
+#clf = GridSearchCV(rfe, parameters)
+#clf.fit(Xbis,ybis)        
+# 
+#n_features_to_select = clf.best_params_ 
+# --> the ideal number of features to select is 207
+
+rfe = RFE(svc, n_features_to_select = 207, step = 20)
+rfe.fit(Xbis,ybis)
 ranking = rfe.ranking_.reshape((28,28)) # le classement des pixels est mis sous forme de matrice numpy
-pl.matshow(ranking) # on peut s'apercevoir de l'importance des pixels du milieu par rapport a ceux des bordures qui ne contiennet aucune info
+pl.matshow(ranking)  # on peut s'apercevoir de l'importance des pixels du milieu par rapport a ceux des bordures qui ne contiennet aucune info
 pl.colorbar()
 pl.title("Ranking of pixels with RFE")
 pl.show()
 
 # I/3 feature elimination
-features_to_keep = np.asarray(np.where(rfe.ranking_ <= 350)).reshape(350) # on garde arbitrairement les 350 pixels le splus explicatifs du label au sens de RFE
+features_to_keep = np.asarray(np.where(rfe.ranking_ == 1)).reshape(207)
 Xbis = X.iloc[:,features_to_keep]
+
 
 # I/4 feature selection using PCA
 pca = PCA(n_components=81) 
@@ -75,32 +92,69 @@ for i in range(nine_first_digit_new.shape[0]):
 
 
 
+#%%
 #-------------------------------- II PREDICTION -----------------------------------------
 from sklearn import svm
 from sklearn.cross_validation import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.lda import LDA
+from sklearn.qda import QDA
 
-# II/1 prediction using support vector machine classification 
-classifier = svm.SVC(gamma = 0.0001)
-X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.33, random_state=42)
-print type(X_train)
-classifier.fit(X_train, y_train) # We learn the digits on the training set 
-y_hat = classifier.predict(X_test) # Now predict the value of the digit on the test set
+X_train, X_test, y_train, y_test = train_test_split(Xbis.values, y.values, test_size=0.66, random_state=42)
+
+# II/1 prediction using support vector machine classification --> classification binaire : inadaptée ici
+#classifier = svm.SVC(gamma = 0.0001)
+#X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.33, random_state=42)
+#print type(X_train)
+#classifier.fit(X_train, y_train) # We learn the digits on the training set 
+#y_hat = classifier.predict(X_test) # Now predict the value of the digit on the test set
 
 
-# II/2 prediction using support k neighborhood classification 
-neigh = KNeighborsClassifier(n_neighbors=10)
+# II/2 prediction using support k neighborhood classification : k odd number is best
+# Using grid.search to determine the best k
+#parameters = {'n_neighbors':[5,7,9,11,13]}
+#neigh = KNeighborsClassifier()
+#clf = GridSearchCV(neigh, parameters)
+#clf.fit(X_train, y_train)        
+# 
+#k = clf.best_params_ #voir comment récupérer automatiquement k
+#clf.best_score_
+#clf.score(X_test, y_test)
+
+# KNN with best value for k: 5
+neigh = KNeighborsClassifier(n_neighbors=5)
 neigh.fit(X_train, y_train) 
+yhat_neigh = neigh.predict(X_test)
+# Error rate
+   # confusion_matrix(y_test, yhat_neigh, labels=None)
+E_neigh = accuracy_score(y_test, yhat_neigh, normalize=True)
+print E_neigh
 
+# II/3 prediction using Linear Discriminant Analysis (LDA)
+LDA = LDA()
+LDA.fit(X_train, y_train)
+yhat_LDA = LDA.predict(X_test)
+E_LDA = accuracy_score(y_test, yhat_LDA, normalize=True)
+print E_LDA
+# KNN better than LDA
 
+# II/3 prediction using Quadratic Discriminant Analysis (QDA)
+QDA = QDA()
+QDA.fit(X_train, y_train)
+yhat_QDA = QDA.predict(X_test)
+E_QDA = accuracy_score(y_test, yhat_QDA, normalize=True)
+print E_QDA
+# voir pb colinéarité des variables
 
-
+#%%
 #-------------------------------- III DEEP LEARNING --------------------------------------
 from theano import *
 
 # III/1 LeNet5 neural network (https://github.com/lisa-lab/DeepLearningTutorials/blob/master/code/convolutional_mlp.py)
 
-
+#%%
 #-------------------------------- IV TESTING ---------------------------------------------
 
 
@@ -153,9 +207,4 @@ for index, (image, prediction) in enumerate(
     pl.title('Prediction: %i' % prediction)
 
 pl.show()
-
-
-
-
-
 
